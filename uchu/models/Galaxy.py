433c85e1e737a1292d6c8641ed9a426011eb7014
HSouch <hsouchereau@outlook.com>
2018-08-24 15:01:28 -0300

    Added I_0, and surface brightness parameters

--- uchu/models/Galaxy.py
+++ uchu/models/Galaxy.py
@@ -3,17 +3,23 @@ class Galaxy:
     """
     Contains all the necessary parameters for a galaxy. 
     """
-    def __init__(self, **kwargs):
-        self.hlr = kwargs["hlr"]
-        self.I_e = kwargs["I_e"]
-        self.n = kwargs["n"]
-        self.r_b = kwargs["r_b"]
-        self.I_b = kwargs["I_b"]
-        self.ipls = kwargs["ipls"]
-        self.opls = kwargs["opls"]
-        self.alpha = kwargs["alpha"]
+    def __init__(self, hlr=None, I_e=None, n=None, r_b=None, I_b=None, ipls=None, opls=None, alpha=None, sb_eff=None,
+                 I_0=None):
+        try:
+            self.hlr = hlr
+        except KeyError:
+            self.hlr = None
+        self.I_e = I_e
+        self.n = n
+        self.r_b = r_b
+        self.I_b = I_b
+        self.ipls = ipls
+        self.opls = opls
+        self.alpha = alpha
+        self.sb_eff = sb_eff
+        self.I_0 = I_0
 
     def get_params(self):
         return {"hlr": self.hlr, "I_e": self.I_e, "n": self.n, "r_b": self.r_b, "I_b": self.I_b, "ipls": self.ipls,
-                "opls": self.opls, "alpha": self.alpha}
+                "opls": self.opls, "alpha": self.alpha, "sb_eff": self.sb_eff, "I_0": self.I_0}
 
