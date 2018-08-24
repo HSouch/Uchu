8fe7a415912e8dfbd6416994032ffafd8873e823
HSouchereau <hsouchereau@outlook.com>
2018-08-23 22:34:52 -0300

    Adjustments

--- tests.py
+++ tests.py
@@ -1,9 +1,22 @@
 from uchu.models.SBModel import *
 from uchu.models.Galaxy import Galaxy
 from uchu.simulation.single_sim import *
+from matplotlib import pyplot as plt
 
 
-galaxy = Galaxy(hlr=55, I_e=50, n=2, r_b=3, I_b=20, ipls=2.5, opls=0.7, alpha=1.3)
+galaxy = Galaxy(hlr=150, I_e=100, n=1.5, r_b=3, I_b=20, ipls=2.5, opls=0.7, alpha=1.3)
 model = sersic_profile
 
-full_galaxy_plot(galaxy, model, 500)
+models = []
+for x in range(1, 6):
+    galaxy.n = x / 2
+    models.append(full_galaxy_plot(galaxy, model, 500))
+
+fig, ax = plt.subplots(1, 5)
+for x in range(0, 5):
+    ax[x].imshow(models[x])
+    readout = "n= " + str((x + 1) / 2) + "\nr_eff= " + str(galaxy.get_params()["hlr"]) + "\nI_eff= " + str(galaxy.get_params()["I_e"])
+    ax[x].text(20, 100, readout, color='white')
+    ax[x].set_xticks([])
+    ax[x].set_yticks([])
+plt.show()
--- uchu/simulation/single_sim.py
+++ uchu/simulation/single_sim.py
@@ -11,10 +11,12 @@ def full_galaxy_plot(galaxy, model, size, noise=0):
     for x in range(0, base_image.shape[0]):
         for y in range(0, base_image.shape[1]):
             base_image[x][y] = model(get_dist((x, y), centre_pix), galaxy.get_params())
-    plt.imshow(base_image)
-    readout = "n= " + str(params["n"]) + "\nr_eff= " + str(params["hlr"]) + "\nI_eff= " + str(params["I_e"])
-    plt.text(20, 100, readout, color='white')
-    plt.show()
+    # plt.imshow(base_image)
+    # readout = "n= " + str(params["n"]) + "\nr_eff= " + str(params["hlr"]) + "\nI_eff= " + str(params["I_e"])
+    # plt.text(20, 100, readout, color='white')
+    # plt.show()
+    return base_image
+
 def model_galaxy(model, params):
     return None
 
